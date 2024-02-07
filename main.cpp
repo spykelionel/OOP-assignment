bf23753d25988487bac5c3d9f75eee6110180a49
spykelionel <social.lionel@gmail.com>
2024-02-07 10:32:52 +0100

    call by reference(&) vs call by address(*)

--- main.cpp
+++ main.cpp
@@ -3,11 +3,22 @@
 
 using namespace std;
 void my_goto();
+void call_by_ref(int &);
+void call_by_addr(int *);
 
 int main()
 {
     cout << "Hello world!" << endl;
-    my_goto();
+    //my_goto();
+
+    int x = 5; // for ref
+    int y = 5; // for addr
+    cout <<"Before Calling by reference, x="<<x<<endl;
+    cout <<"Before Calling by address, y="<<y<<endl;
+    call_by_ref(x);
+    call_by_addr(&y);
+    cout <<"After Calling by reference, x="<<x<<endl;
+    cout <<"After Calling by address, y="<<y<<endl;
     return 0;
 }
 
@@ -21,4 +32,12 @@ void my_goto(){
         if(num!=MAX_GUESS)goto guess;
         cout<< "You guessed " <<num << " Correctly"<<endl;
 
+}
+
+void call_by_ref(int &x){
+    x = x + 10;
+}
+
+void call_by_addr(int *y){
+    *y = *y + 10;
 }
